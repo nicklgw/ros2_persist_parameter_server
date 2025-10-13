9ff289b81976158ea5d6a1e0d0acc3c8d4dee398
liguowang <liguowang01@countrygarden.com.cn>
2025-10-13 14:52:25 +0800

    use cyclonedds.xml

--- server/launch/parameter_server.sh
+++ server/launch/parameter_server.sh
@@ -5,8 +5,6 @@ source /opt/ros/humble/setup.bash
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export CYCLONEDDS_URI=file:///etc/cyclonedds.xml
 
-export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-
 mkdir -m a=rwx -p /var/log/bzlrobot/parameter_server
 export ROS_LOG_DIR="/var/log/bzlrobot/parameter_server"
 
