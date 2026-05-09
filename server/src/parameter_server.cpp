081fcc42bfbc5c2c96af0237f9a1195b91e17f8c
nicklgw <nicklgw@aliyun.com>
2026-05-09 15:35:26 +0800

    确保数据写入磁盘

--- server/src/parameter_server.cpp
+++ server/src/parameter_server.cpp
@@ -592,6 +592,7 @@ void ParameterServer::StoreYamlFile()
     std::ofstream fout(persistent_yaml_file_);
     fout << out.c_str();
     RCLCPP_INFO(this->get_logger(), "parameter_config \n %s", out.c_str());
+    fout.flush();  // 刷新 C++ 缓冲区
     fout.close();
 
     {
